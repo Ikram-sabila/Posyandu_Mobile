903f427d99d5b31421132c40c376a8e3b1de58cd
raniaJilan <126790334+raniaJilan@users.noreply.github.com>
2025-05-21 14:10:50 +0700

    ButtonBar

--- app/src/main/java/com/example/posyandu/ui/Screen/components/UiComponents.kt
+++ app/src/main/java/com/example/posyandu/ui/Screen/components/UiComponents.kt
@@ -1,13 +1,24 @@
 package com.example.posyandu.ui.Screen.components
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.DateRange
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Person
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -68,6 +79,55 @@ fun InfoCard(title: String, value: String, bgColor: Color, icon: String) {
             Column {
                 Text(title, fontSize = 12.sp)
                 Text(value, fontWeight = FontWeight.Bold)
+            }
+        }
+    }
+}
+@Composable
+fun ButtonBar(
+    selectedIndex: Int,
+    onItemSelected: (Int) -> Unit
+) {
+    val items = listOf(
+        Icons.Default.Home,
+        Icons.Default.Favorite,
+        Icons.Default.DateRange,
+        Icons.Default.Person
+    )
+
+    val description = listOf("Home", "Heart", "Calendar", "Profile")
+
+    Surface(
+        tonalElevation = 10.dp, // memberikan bayangan halus
+        shadowElevation = 10.dp, // ini yang utama untuk efek garis atas
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(Color.White)
+                .padding(vertical = 12.dp),
+            horizontalArrangement = Arrangement.SpaceEvenly,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            items.forEachIndexed { index, icon ->
+                Box(
+                    modifier = Modifier
+                        .size(if (index == selectedIndex) 56.dp else 40.dp)
+                        .clip(CircleShape)
+                        .background(
+                            if (index == selectedIndex) Color(0xFF0F6674) else Color.Transparent
+                        )
+                        .clickable { onItemSelected(index) },
+                    contentAlignment = Alignment.Center
+                ) {
+                    Icon(
+                        imageVector = icon,
+                        contentDescription = description[index],
+                        tint = if (index == selectedIndex) Color.White else Color(0xFF0F6674),
+                        modifier = Modifier.size(32.dp)
+                    )
+                }
             }
         }
     }
